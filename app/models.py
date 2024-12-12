01bbff35f5b9e1c263ab63b7736d80a5021d8c5e
rooong1124 <carl100113@gmail.com>
2024-12-12 15:55:27 +0800

    Update users model

--- app/models.py
+++ app/models.py
@@ -5,7 +5,15 @@ from .database import Base
 class Todo(Base):
     __tablename__ = "todos"
     id = Column(Integer, primary_key=True)
-    title = Column(String[100], nullable=False)
+    title = Column(String(100), nullable=False)
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
